bb8c14f2d66af12b6772ac5eea21248dd077e4b7
philipd <phildkh97@gmail.com>
2021-12-03 16:25:01 +0800

    feat(user): add bcrypt and migration for password field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_150116) do
+ActiveRecord::Schema.define(version: 2021_12_03_075944) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_10_27_150116) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
