f58220b59c0e378d9e2e13684c63d76fdbbc8023
philipd <phildkh97@gmail.com>
2022-01-20 05:08:09 +0800

    feat: add admin user requirement for category with test case

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -7,11 +7,15 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    @admin_user = User.create(username: "johndoe", email: "john@example.com", password: "password", admin: true)
+    sign_in_as(@admin_user)
     get new_category_url
     assert_response :success
   end
 
   test "should create category" do
+    @admin_user = User.create(username: "johndoe", email: "john@example.com", password: "password", admin: true)
+    sign_in_as(@admin_user)
     assert_difference('Category.count', 1) do
       post categories_url, params: { category: { name: "Travel" } }
     end
@@ -23,6 +27,14 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     @category = Category.create(name: "Sports")
     get category_url(@category)
     assert_response :success
+  end
+
+  test "should not create category if not admin" do
+    assert_no_difference('Category.count') do
+      post categories_url, params: { category: { name: "Travel" } }
+    end
+
+    assert_redirected_to categories_url
   end
 
   # test "should get edit" do
