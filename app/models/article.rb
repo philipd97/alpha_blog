ede0d742173a6faf9852a773d08eaa67812717bf
philipd <phildkh97@gmail.com>
2022-03-13 17:37:04 +0800

    refactor: use has_and_belongs_to_many for many to many relationship instead of using a join model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,5 @@
 class Article < ApplicationRecord
-  belongs_to :user
-  has_many :article_categories
-  has_many :categories, through: :article_categories
+  has_and_belongs_to_many :categories
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 6, maximum: 100 }
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
-  has_many :article_categories
-  has_many :articles, through: :article_categories
+  has_and_belongs_to_many :articles
   validates :name, presence: true, length: { minimum:3, maximum:25 }
   validates_uniqueness_of :name
 end
