f15dcd50e2a81080d0be5bc030cd7024082af34a
philipd <phildkh97@gmail.com>
2022-03-27 20:34:29 +0800

    refactor: revert back join model for articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :categories
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 6, maximum: 100 }
 end
